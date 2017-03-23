09c897400f899554cc8d015279d073595eff84d9
Richard Neher <richard.neher@tuebingen.mpg.de>
2017-03-23 21:04:54 +0100

    set n.clade if not set -- now correct

--- src/phyloTree.js
+++ src/phyloTree.js
@@ -28,7 +28,7 @@ const phyloTree = function(treeJson, params) {
           }
         }
         //add clade (serves as a unique node identifier) if not set
-        if (!node.n.clade){node.n.clade = nodeArray.length+1;}
+        if (!node.clade){node.clade = nodeArray.length+1;}
         nodeArray.push(node);
     }
     if (!treeJson.attr){
